f4099f9a045a13aa36fe69ba7a07bd13c250334a
@designervenkat <murty.av@gmail.com>
2024-11-07 10:28:46 +0000

    design fix

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,27 +1,38 @@
+import Link from "next/link"
+
 export default async function ProductDetails(
-  { params, searchParams }: { params: { productId: number }, searchParams: Promise<{ [key:string]: string | string[] | undefined}> }
-  // { params }: {params: Promise<{productId: string}>} 
+  {
+    
+    params,
+    searchParams
+
+  } : {
+      
+      params: Promise<{ productId: string }>,
+      searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+
+    }
 ) {
-  console.log(params);
   // props prameters
   const { productId } = await params
-  // const productId = (await params).productId
 
   // search parameters
   const { name, storage, color } = await searchParams
-   
-  // const name = (await searchParams).name
-  //  const storage = (await searchParams).storage
-  //  const color = (await searchParams).color
 
   return (
-    <div>
-      Product Detail Page for <br />
+    <div className="grid place-content-center">
+
+      <h2 className="my-5">Product Detail Page</h2>
       
-      Product Number - {productId} <br />
-      Product Name - {name} <br />
-      Product Storage = {storage} <br />
-      Product Color =  {color}
+      <div className="bg-slate-800 p-5 grid grid-cols-2 gap-6 text-xs text-slate-50">
+        {name && <p>Product Name - {name} </p>}
+        {productId && <p>Product Id - {productId} </p>}
+        {storage && <p>Product Storage - {storage} </p>}
+        {color && <p>Product Color - {color} </p>}
+      </div>
+
+      <Link className="bg-green-200 text-green-800 text-xs px-8 py-2 w-36 my-5 rounded-sm" href={`/products/10/reviews/10`}>Check Review</Link>
+        
     </div>
   )
 }
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,21 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
-
-type Params = Promise<{
-
-    productId: string;
-    reviewId: string;
-
-}>;
 
 function getRandomNum(num: number) {
   return Math.floor(Math.random() * num)
-} 
+}
 
-export default async function Reviews({ params }: {params: Params}) {
-  
+export default async function Reviews(
+  { params }: { params: Promise<{ productId: string, reviewId: string }>, }) {
+
   const { productId, reviewId } = await params
-  
-   const random = getRandomNum(2)
+
+  const random = getRandomNum(2)
 
   if (random === 1) {
     throw new Error("Errow loading review")
@@ -24,7 +19,9 @@ export default async function Reviews({ params }: {params: Params}) {
 
   return (
     <div className='h-screen grid place-content-center'>
-      <h1 className='text-4xl'>Review {reviewId} for product { productId}</h1>
+      <h1 className='text-4xl'>Review {reviewId} for product {productId}</h1>
+
+      <Link className="bg-green-200 text-green-800 text-xs px-8 py-2 w-40 my-5 rounded-sm" href={`/products/10?name=Macbook&storage=16gb&color=SpaceGray`}>Product Details</Link>
     </div>
   )
 }
