200a537dbd14ed450725a1b393eaec24b0e0b25c
@designervenkat <murty.av@gmail.com>
2024-11-13 14:31:47 +0000

    modified

--- src/components/Reports.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Reports() {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-  return (
-    <h1 className="text-2xl">
-      Reports Page
-    </h1>
-  )
-}
