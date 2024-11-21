ce386b60c95b56502f446cdf5626d38b66aad668
@designervenkat <murty.av@gmail.com>
2024-11-21 17:15:03 +0000

    CSP Security Policies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,47 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              'camera=(self "https://www.megabyte.mu"), microphone=(self "https://www.megabyte.mu"), geolocation=(self "https://www.megabyte.mu"), microphone=(self "https://www.megabyte.mu")',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.megabyte.mu',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: '',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
